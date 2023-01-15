9983259506e74583cf68533cac8af3ba91f25e82
debussy2k <harry@motion1.co.kr>
2023-01-16 01:27:01 +0900

    wip. add undef  HB_NO_LAYOUT_FEATURE_PARAMS

--- config-override.h
+++ config-override.h
@@ -6,3 +6,11 @@
 #undef HB_NO_VAR
 #undef HB_NO_OT_FONT_GLYPH_NAMES
 #undef HB_NO_FACE_COLLECT_UNICODES
+
+/*
+ * for using
+ 	hb_ot_layout_get_size_params
+	hb_ot_layout_feature_get_name_ids
+	hb_ot_layout_feature_get_characters
+*/ 
+#undef HB_NO_LAYOUT_FEATURE_PARAMS
